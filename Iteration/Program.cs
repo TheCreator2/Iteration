319acfcacc85298588e3d6e84622bfe4ab03cf52
Samuel Budge <17BudgeS@fulford.york.sch.uk>
2022-09-12 15:17:17 +0100

    stack overflow came in clutch

--- Iteration/Program.cs
+++ Iteration/Program.cs
@@ -10,9 +10,9 @@ namespace Iteration
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i < 100; i++) 
+            for (int i = 0; i < 100; i++)
             {
-                Console.WriteLine("I have eaten " +  i + "pizza(s)");
+                Console.WriteLine("I have eaten " + i + "pizza(s)");
             }
 
             bool areNearlyThereYet = false;
@@ -23,6 +23,40 @@ namespace Iteration
                 areNearlyThereYet = Console.ReadLine().ToLower() == "yes";
             }
 
+
+            //reapeat from 1 - 50
+            for (int i = 1; i < 50; i++)
+            {
+                string oddOrEven = "odd";
+                if (i % 2 == 0)
+                {
+                    oddOrEven = "even";
+                }
+                else
+                {
+                    oddOrEven = "odd";
+                }
+                Console.WriteLine($"{i} is {oddOrEven}");
+            }
+
+            //see if number is divisiblr by 3
+            Console.WriteLine("Gimmie a number in times table");
+            int num = int.Parse(Console.ReadLine());
+
+
+            for (int i = 1; i < 50; i++)
+            {
+                string isInTable = "odd";
+                if (i % num == 0)
+                {
+                    isInTable = "oui oui";
+                }
+                else
+                {
+                    isInTable = "nien";
+                }
+                Console.WriteLine($"{i} is {isInTable}");
+            }
         }
     }
 }
