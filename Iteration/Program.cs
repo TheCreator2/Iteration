dc80d8303574fc4023f5faddb79ef7151b4cb4e4
Samuel Budge <17BudgeS@fulford.york.sch.uk>
2022-09-12 14:57:04 +0100

    Added code

--- Iteration/Program.cs
+++ Iteration/Program.cs
@@ -10,6 +10,19 @@ namespace Iteration
     {
         static void Main(string[] args)
         {
+            for (int i = 0; i < 100; i++) 
+            {
+                Console.WriteLine("I have eaten " +  i + "pizza(s)");
+            }
+
+            bool areNearlyThereYet = false;
+
+            while (areNearlyThereYet == false)
+            {
+                Console.WriteLine("Are we nearly there yet?");
+                areNearlyThereYet = Console.ReadLine().ToLower() == "yes";
+            }
+
         }
     }
 }
